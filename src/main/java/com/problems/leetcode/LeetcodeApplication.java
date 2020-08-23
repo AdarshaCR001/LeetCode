f366f775727cf0d764ffdac0ef0369784ba39067
Adarsha C R <adarsha@cloudthing.com>
2020-08-23 19:10:21 +0530

    First commit to setup application

---
+++ src/main/java/com/problems/leetcode/LeetcodeApplication.java
@@ -0,0 +1,15 @@
+package com.problems.leetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class LeetcodeApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(LeetcodeApplication.class, args);
+
+		System.out.println("Hello wolrd");
+	}
+
+}
