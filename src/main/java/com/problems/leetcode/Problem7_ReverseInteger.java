c87f2fd625f3550025b7ee3a88d58d2d2fc79c76
Adarsha C R <adarsha@cloudthing.com>
2020-08-26 19:17:02 +0530

    Solved problems 7, 8 & 9.

---
+++ src/main/java/com/problems/leetcode/Problem7_ReverseInteger.java
@@ -0,0 +1,21 @@
+package com.problems.leetcode;
+
+public class Problem7_ReverseInteger
+{
+    public int Reverse(int x)
+    {
+        int sum = 0;
+        int first = 0;
+        while (x != 0)
+        {
+            first = x % 10;
+            sum = sum * 10 + first;
+            x = x / 10;
+        }
+        if (first != sum % 10)
+        {
+            return 0;
+        }
+        return sum;
+    }
+}
